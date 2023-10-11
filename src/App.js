b2d02ffb1261f51d06042b3e142e49691d9a68d5
gun1kim <obkh31@naver.com>
2023-10-12 00:46:59 +0900

    react kakaopay test

--- src/App.js
+++ src/App.js
@@ -1,60 +1,73 @@
-// import React from "react";
-// import axios from "axios";
 // import { useState, useEffect } from "react";
+// import axios from "axios";
+
+// const kpConfig = {
+//   next_redirect_pc_url: "",
+//   tid: "",
+//   params: {
+//     cid: "TC0ONETIME",
+//         partner_order_id: "partner_order_id",
+//         partner_user_id: "partner_user_id",
+//         item_name: "테스트",
+//         quantity: 1,
+//         total_amount: 10000,
+//         vat_amount: 200,
+//         tax_free_amount: 0,
+//         approval_url: "http://localhost:3000/",
+//         fail_url: "http://localhost:3000/",
+//         cancel_url: "http://localhost:3000/",
+//   },
+// };
 
 // function App() {
-//   let [data, setData] = useState([]);
+//   const [data, setData] = useState({});
+//   const { params } = kpConfig;
+//   const payData = async () => {
+//     const URL = "/v1/payment/ready";
+//     // let next_redirect_pc_url = "";
+//     // let tid = "";
+//     await axios.post(
+//       URL, params, {
+//       // {
+//       //   next_redirect_pc_url: "",
+//       //   tid: "",
 
-
-//    data = {
-//     next_redirect_pc_url: "",
-//     tid: "",
-//     params: {
-//       cid: "TC0ONETIME",
-//       partner_order_id: "partner_order_id",
-//       partner_user_id: "partner_user_id",
-//       item_name: "테스트",
-//       quantity: 1,
-//       total_amount: 10000,
-//       vat_amount: 200,
-//       tax_free_amount: 0,
-//       approval_url: "http://localhost:3000/",
-//       fail_url: "http://localhost:3000/",
-//       cancel_url: "http://localhost:3000/",
-//     },
-//   };
-
-//   useEffect(() => {
-//     const { params } = data;
-
-//     axios({
-//       url: "/v1/payment/ready",
-//       mehtod: "POST",
+//       // params: {
+//       //   cid: "TC0ONETIME",
+//       //   partner_order_id: "partner_order_id",
+//       //   partner_user_id: "partner_user_id",
+//       //   item_name: "테스트",
+//       //   quantity: 1,
+//       //   total_amount: 10000,
+//       //   vat_amount: 200,
+//       //   tax_free_amount: 0,
+//       //   approval_url: "http://localhost:3000/",
+//       //   fail_url: "http://localhost:3000/",
+//       //   cancel_url: "http://localhost:3000/",
+//       // },
 //       headers: {
 //         Authorization: "KakaoAK ae3ef7e66d1721cd8c214020d71aa988",
 //         "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
-//       },
+//       }, 
 //       params,
 //     }).then((response) => {
 //       const {
-//         data: { next_redirect_pc_url, tid }
+//         data: {next_redirect_pc_url, tid}
 //       } = response;
-
 //       console.log(next_redirect_pc_url);
 //       console.log(tid);
-//       this.setData({ next_redirect_pc_url, tid });
+//       this.setData({ next_redirect_pc_url, tid});
 //     });
-//   }, []);
-
-
-
-
-//   return (<div>
-//     <h2>Pay page</h2>
-//     {/* <a href={next_redirect_pc_url}> */}
-//       {/* {next_redirect_pc_url} */}
-//     {/* </a> */}
-//   </div>);
+//   }
+//   payData();
+//   return (
+//     <div>
+//       <h2>pay page</h2>
+//       <a href={ next_redirect_pc_url }>
+//         {next_redirect_pc_url}
+//       </a>
+//     </div>
+//   );
 // }
 
 // export default App;
@@ -62,19 +75,11 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-function App() {
-  const [data, setData] = useState({});
-  const payData = async () => {
-    const URL = "/v1/payment/ready";
-    // let next_redirect_pc_url = "";
-    // let tid = "";
-    await axios.post(
-      URL, {
-        next_redirect_pc_url: "",
-        tid: "",
-
-      params: {
-        cid: "TC0ONETIME",
+const kpConfig = {
+  next_redirect_pc_url: "",
+  tid: "",
+  params: {
+    cid: "TC0ONETIME",
         partner_order_id: "partner_order_id",
         partner_user_id: "partner_user_id",
         item_name: "테스트",
@@ -85,27 +90,38 @@ function App() {
         approval_url: "http://localhost:3000/",
         fail_url: "http://localhost:3000/",
         cancel_url: "http://localhost:3000/",
-      },
-      headers: {
-        Authorization: "KakaoAK ae3ef7e66d1721cd8c214020d71aa988",
-        "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
-      }, 
-      params,
-    }).then((response) => {
-      const {
-        data: {next_redirect_pc_url, tid}
-      } = response;
-      console.log(next_redirect_pc_url);
-      console.log(tid);
-      this.setData({ next_redirect_pc_url, tid});
-    });
-  }
+  },
+};
 
+function App() {
+  const [data, setData] = useState({});
+  const { params } = kpConfig;
+  
+  useEffect(() => {
+    const postKakaopay = async () => {
+      const data = await axios.post('/v1/payment/ready', null, {
+        params,
+        headers: {
+          Authorization: "KakaoAK ae3ef7e66d1721cd8c214020d71aa988",
+          "Content-Type": "application/x-www-form-urlencoded; charset=utf-8"
+        }
+      })
+      .then((response) => {
+        const {
+          data: {next_redirect_pc_url, tid}
+        } = response;
+        setData({next_redirect_pc_url, tid});
+        console.log(response);
+      });
+    }
+    postKakaopay();
+  }, [])
   return (
+    // <></>
     <div>
       <h2>pay page</h2>
-      <a href={ next_redirect_pc_url }>
-        {next_redirect_pc_url}
+      <a href={ data.next_redirect_pc_url }>
+        {data.next_redirect_pc_url}
       </a>
     </div>
   );
